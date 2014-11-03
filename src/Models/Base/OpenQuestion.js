e7178ac02eb3f5690e82c01e2133ab47cab6c66d
Dmitriy Dzyuba <dmitriy.dzyuba@smoozeedev.com>
2014-11-03 13:36:01 +0200

    limit option validation for open questions

--- src/Models/Base/OpenQuestion.js
+++ src/Models/Base/OpenQuestion.js
@@ -8,5 +8,24 @@ applr.Models.Base.OpenQuestion = applr.Models.Base.Question.extend({
 		if (attr !== undefined && attr.options !== undefined && attr.options.name == undefined) {
 			this.attributes.options.name = _generateName();
 		}
+	},
+
+	validate: function(attrs) {
+		//checking limit
+		var
+			min_limit,
+			max_limit
+		;
+		if (attrs.view == 'Textfield') {
+			min_limit = 1;
+			max_limit = _textfieldMaxLimit;
+		} else if (attrs.view == 'Textarea') {
+			min_limit = _textfieldMaxLimit + 1;
+			max_limit = _textareaMaxLimit;
+		}
+
+		if (attrs.options.limit > max_limit || attr.options.limit < min_limit) {
+			return 'Incorrect value for limit field';
+		}
 	}
 });
